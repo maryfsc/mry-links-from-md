38db51f6059f25096b64cc2fa2d5f63b9ab640d6
Mary Francis <maryfsc@disroot.org>
2018-09-03 21:18:14 -0300

    Adicionando testes e removendo verificação desnecessária

--- index.js
+++ index.js
@@ -7,31 +7,29 @@ module.exports.getLinksFromMd = function getLinksFromMd(md) {
     throw new Error('Parameter must be a string');
   }
 
-  if (typeof md === 'string') {
-    let regexUrl = new RegExp (/(http:\/\/|https:\/\/)?(w{3}\.)?(\w)+(\.(\w){2,3}){1,2}/g);
-    let regexAlt = new RegExp (/(?<=\[).*?(?=\])/g);
+  let regexUrl = new RegExp (/(http:\/\/|https:\/\/)?(w{3}\.)?(\w)+(\.(\w){2,3}){1,2}/g);
+  let regexAlt = new RegExp (/(?<=\[).*?(?=\])/g);
 
-    let textUrl = md.match(regexUrl);
-    let textAlt = md.match(regexAlt);
+  let textUrl = md.match(regexUrl);
+  let textAlt = md.match(regexAlt);
 
-    let obj = {};
-    let result = [];
+  let obj = {};
+  let result = [];
 
-    if (textUrl === null) {
-      return [];
+  if (textUrl === null) {
+    return [];
+  }
+
+  let output = textUrl.map((url, i) => {
+    obj = {
+      href: url,
+      text: textAlt[i]
     }
 
-    let output = textUrl.map((url, i) => {
-      obj = {
-        href: url,
-        text: textAlt[i]
-      }
+    result.push(obj);
+  });
 
-      result.push(obj);
-    });
+  return result;
+} 
 
-    return result;
-  } 
-}
 
-let str = 'It\'s very easy to make some words **bold** and other words *italic* with Markdown. You can even [link to Google!](http://google.com) , It\'s very easy to make some words **bold** and other words *italic* with Markdown. You can even [eita!](http://github.com) , It\'s very easy to make some words **bold** and other words *italic* with Markdown. You can even [mexerica](http://banana.com)'
--- test/index.spec.js
+++ test/index.spec.js
@@ -3,10 +3,52 @@ const chai = require('chai');
 const lib = require('../index');
 const expect = chai.expect;
 
-describe('#getLinksFromMd', function () {
-  describe('when there is no parameter'), function () {
+describe('getLinksFromMd', function () {
+  describe('when there is no parameter', function () {
     it('should throw an error', function () {
-      expect(function() { lib.getLinksFromMd() }).to.throw(new Error('No parameter given'));
+      expect(() => { lib.getLinksFromMd() }).to.throw('No parameter given');
     });
-  }
+  });
+  describe('when parameter is a number', function () {
+    it('should throw an error', function () {
+      expect(() => { lib.getLinksFromMd(5) }).to.throw('Parameter must be a string');
+    });
+  });
+  describe('when parameter is a text without links', function () {
+    it('should return an empty array', function () {
+    	const text = 'Laboratoria is super cool!';
+      expect(lib.getLinksFromMd(text)).be.an('array');
+      expect(lib.getLinksFromMd(text)).be.empty;
+    });
+  });
+  describe('when there\'s just a link in markdown', function () {
+    it('should return an array with an object', function () {
+    	const singleLink = 'Get to know [Laboratoria](https://laboratoria.la)!'
+      expect(lib.getLinksFromMd(singleLink)).to.deep.equal([{href: 'https://laboratoria.la', text: 'Laboratoria'}]);
+    });
+  });
+  describe('when there\'s two links in markdown', function () {
+    it('should return an array with both objects', function () {
+    	const twoLinks = 'Get to know [Laboratoria](https://laboratoria.la) and [Google](https://google.com)!'
+      expect(lib.getLinksFromMd(twoLinks)).to.deep.equal([{href: 'https://laboratoria.la', text: 'Laboratoria'}, {href: 'https://google.com', text: 'Google'} ]);
+    });
+  });
+  describe('when there\'s three links in markdown', function () {
+    it('should return an array with three objects', function () {
+    	const threeLinks = 'Get to know [Laboratoria](https://laboratoria.la) and [Google](https://google.com) and also [StackOverflow](https://stackoverflow.com)!'
+      expect(lib.getLinksFromMd(threeLinks)).to.deep.equal([{href: 'https://laboratoria.la', text: 'Laboratoria'}, {href: 'https://google.com', text: 'Google'}, {href: 'https://stackoverflow.com', text: 'StackOverflow'} ]);
+    });
+  });
+  describe('when there\'s a link with http', function () {
+    it('should return an array with the objects', function () {
+    	const sameThreeLinks = 'Get to know [Laboratoria](https://laboratoria.la) and [Google](http://google.com) and also [StackOverflow](https://stackoverflow.com)!'
+      expect(lib.getLinksFromMd(sameThreeLinks)).to.deep.equal([{href: 'https://laboratoria.la', text: 'Laboratoria'}, {href: 'http://google.com', text: 'Google'}, {href: 'https://stackoverflow.com', text: 'StackOverflow'} ]);
+    });
+  });
+  describe('when there\'s just the domain', function () {
+    it('should return an array with the objects', function () {
+    	const sameThreeLinksAgain = 'Get to know [Laboratoria](www.laboratoria.la) and [Google](http://google.com) and also [StackOverflow](https://stackoverflow.com)!'
+      expect(lib.getLinksFromMd(sameThreeLinksAgain)).to.deep.equal([{href: 'www.laboratoria.la', text: 'Laboratoria'}, {href: 'http://google.com', text: 'Google'}, {href: 'https://stackoverflow.com', text: 'StackOverflow'} ]);
+    });
+  });        
 });
